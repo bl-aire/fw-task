ad18eeecc725b464e37dcba63ac9ef045637c9b5
Blaire <ogoh_blessing@yahoo.com>
2022-07-16 21:21:25 +0100

    Add server-side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     NavigationComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule
   ],
